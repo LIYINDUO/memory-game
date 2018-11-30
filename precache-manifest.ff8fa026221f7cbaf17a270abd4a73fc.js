9eaf8215800bfec009d045fac8f83f921ce4e698
EC2 Default User <ec2-user@ip-172-31-38-98.ap-southeast-1.compute.internal>
2018-11-30 14:33:08 +0000

    Updates

---
+++ precache-manifest.ff8fa026221f7cbaf17a270abd4a73fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "c070cfb335305b744e16",
+    "url": "./static/js/main.c070cfb3.chunk.js"
+  },
+  {
+    "revision": "5ebaaf0a9da1127a21cb",
+    "url": "./static/js/1.5ebaaf0a.chunk.js"
+  },
+  {
+    "revision": "c070cfb335305b744e16",
+    "url": "./static/css/main.f64b755d.chunk.css"
+  },
+  {
+    "revision": "c5152dcf9290008e705d65d03e2f7d94",
+    "url": "./index.html"
+  }
+];
